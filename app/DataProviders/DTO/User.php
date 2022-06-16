07821035d86bcb4a669502de9046b1652b5a93d1
AbdelrahmanAmin <abdelrahman.amin93@gmail.com>
2022-06-16 21:45:33 +0200

    Add User DTO

---
+++ app/DataProviders/DTO/User.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataProviders\DTO;
+
+class User
+{
+
+    public $data_provider_id;
+
+    public $data_provider;
+
+    public $amount;
+
+    public $currency;
+
+    public $email;
+
+    public $status_code;
+
+    public $created_at;
+
+}
