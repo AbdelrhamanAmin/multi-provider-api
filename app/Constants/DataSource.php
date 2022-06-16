a4b24ff5760432a67819ed9540e9b5bd0845432b
AbdelrahmanAmin <abdelrahman.amin93@gmail.com>
2022-06-16 21:48:12 +0200

    Add data source files and constants

---
+++ app/Constants/DataSource.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Constants;
+
+class DataSource
+{
+
+    const DATA_PROVIDER_X = 'DataProviderX';
+    const DATA_PROVIDER_Y = 'DataProviderY';
+
+    /**
+     *Available Data providers
+    */
+    const DATA_PROVIDERS = [
+        self::DATA_PROVIDER_X,
+        self::DATA_PROVIDER_Y
+    ];
+
+}
